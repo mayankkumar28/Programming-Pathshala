8f7a07bba2bee5ba018d0178959301404e87c0ad
Mayank Kumar <mayank.zxc@gmail.com>
2021-04-18 12:51:40 +0530

    Leetcode 42

--- src/Leetcode_42.java
+++ src/Leetcode_42.java
@@ -22,7 +22,6 @@ public class Leetcode_42 {
         }
         return amount;
     }
-
     public static void main(String[] args){
         int[] height = {0,1,0,2,1,0,1,3,2,1,2,1};
         System.out.println(trap(height));
