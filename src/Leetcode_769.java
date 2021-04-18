9127a90978a617b6e1b4e40917d6b5d792800401
Mayank Kumar <mayank.zxc@gmail.com>
2021-04-18 23:01:21 +0530

    Arrays

--- src/Leetcode_769.java
+++ src/Leetcode_769.java
@@ -12,7 +12,7 @@ public class Leetcode_769 {
         return chunks;
     }
     public static void main(String[] args){
-        int[] arr = {4,3,2,1,0};
+        int[] arr = {5,4,3,2,1,0};
         System.out.println(maxChunksToSorted(arr));
     }
 }
