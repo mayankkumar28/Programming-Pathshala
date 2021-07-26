5a1382d380fa8db021d8d72224fdc182839a5e9d
Mayank Kumar <mayank.zxc@gmail.com>
2021-07-26 18:14:34 +0530

    added comments

--- src/Arrays_Codeforces_740B_AlyonaFlowers.java
+++ src/Arrays_Codeforces_740B_AlyonaFlowers.java
@@ -1,4 +1,12 @@
 import java.util.Scanner;
+/*
+The Question is complicated. Basically we have to determine a max output from different chosen sub-arrays
+So instead of taking two indexes and iterating and summing each time, we precompute the sum of array from left to right
+We then take a temp int where we store the sum of the sub-array, we have the choice to use or discard it
+If the sum is positive we chose it, otherwise we discard it, we can discard all of them if we want
+We move to next sub-array and keep adding to the sum
+ */
+
 
 public class Arrays_Codeforces_740B_AlyonaFlowers {
     public static void main(String[] args){
@@ -9,16 +17,16 @@ public class Arrays_Codeforces_740B_AlyonaFlowers {
         int[] sum = new int[n+1];
         for (int i=1;i<=n;i++){
             arr[i]= sc.nextInt();
-            sum[i] = arr[i]+sum[i-1];
+            sum[i] = arr[i]+sum[i-1];  //Computing array sum from left to right
         }
 
         int max = 0;
         for (int i =1; i<=m;i++){
-            int left =sc.nextInt();
-            int right =sc.nextInt();
+            int left =sc.nextInt();  //Left index of sub-array
+            int right =sc.nextInt(); //Right index of sub-array
             int temp =0;
-            temp = sum[right]-sum[left-1];
-            if (temp>0){
+            temp = sum[right]-sum[left-1];  //Calculating sub-array sum.
+            if (temp>0){   //We choose it if +ve
                 max += temp;
             }
         }
--- src/Arrays_GFG_RowMax1s.java
+++ src/Arrays_GFG_RowMax1s.java
@@ -1,25 +1,25 @@
 //https://practice.geeksforgeeks.org/problems/row-with-max-1s0023/1
-
+/*
+We are finding row with maximum number of 1's
+we start from top right as we can determine next possible direction from there. bottom left is possible too
+If the present position has one in it. we continue left otherwise shift down
+Loop ends when we reach the last row or last col
+ */
 public class Arrays_GFG_RowMax1s {
     static int rowWithMax1s(int[][] arr, int n, int m) {
-        int i = 0;
-        int j = m-1;
+        int row = 0;
+        int col = m-1;
         int index=-1;
-        int lowest_j=j;
-        while(i<=n-1 && j>=0){
-            if (arr[i][j]==1){
-                j--;
-                if (lowest_j>j){
-                    lowest_j=j;
-                    index=i;
-                }
+        while(row<=n-1 && col>=0){   //Keeping in boundaries
+            if (arr[row][col]==1){   //Found one
+                col--;   //Shifting left
+                index=row;  //new ans index
             }
             else {
-                j=m-1;
-                i++;
+                row++;   //didn't found 1, shifting down
             }
         }
-        return index;
+        return index; 
     }
     public static void main(String[] args){
         int[][] arr = {{0, 1, 1, 1},
